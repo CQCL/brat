332c20acafbba8ef9bc73b126efc65865fd59a3b
Craig Roy <craig.roy@strath.ac.uk>
2022-02-02 15:58:07 +0000

    [tests] Add more tests of circuit generation

--- brat/test/Test/Circuit.hs
+++ brat/test/Test/Circuit.hs
@@ -26,16 +26,60 @@ processTest :: Graph' Term -> (Row Term Quantum, Row Term Quantum) -> [Command] 
 processTest g io c = commands (process g io) @?= c
 
 testId = testCase "id" $
-  let g = ([KernelNode "id" (Prim "X") [("xq_in", Q Qubit)] [("xq_out", Q Qubit)]
-           --,KernelNode "eval" (Eval "") testProcess
-           ,BratNode "main" ("src" :>>: "tgt") [] [("fun", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))]
+  let g = ([BratNode "main" ("src" :>>: "tgt") [] [("fun", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))]
            ,KernelNode "src" Source [] [("a", Q Qubit)]
            ,KernelNode "tgt" Target [("b", Q Qubit)] []
            ]
           ,[(("src", "a"), Left (Q Qubit), ("tgt", "b"))]
           )
+      c = []
+  in processTest g (R [("in", Q Qubit)], R [("out", Q Qubit)]) c
+
+testId2 = testCase "id2" $
+  let g = ([KernelNode "id" (Prim "X") [("xq_in", Q Qubit)] [("xq_out", Q Qubit)]
+           --,KernelNode "eval" (Eval "") testProcess
+           ,BratNode "main" ("src" :>>: "tgt") [] [("fun"
+                                                   , K
+                                                     (R [("ina", Q Qubit),  ("inb", Q Qubit)])
+                                                     (R [("outa", Q Qubit), ("outb", Q Qubit)]))]
+           ,KernelNode "src" Source [] [("a", Q Qubit), ("b", Q Qubit)]
+           ,KernelNode "tgt" Target [("a", Q Qubit), ("b", Q Qubit)] []
+           ]
+          ,[(("src", "a"), Left (Q Qubit), ("tgt", "a"))
+           ,(("src", "b"), Left (Q Qubit), ("tgt", "b"))
+           ]
+          )
+      c = []
+  in processTest g (R [("ina", Q Qubit), ("inb", Q Qubit)], R [("outa", Q Qubit), ("outb", Q Qubit)]) c
+
+testX = testCase "X" $
+  let g = ([BratNode "tket.X" (Prim "tket.X") [("kernel", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))] []
+           ,KernelNode "X" (Eval ("tket.X", "kernel")) [("a", Q Qubit)] [("b", Q Qubit)]
+           ,BratNode "main" ("src" :>>: "tgt") [] [("fun", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))]
+           ,KernelNode "src" Source [] [("a", Q Qubit)]
+           ,KernelNode "tgt" Target [("b", Q Qubit)] []
+           ]
+          ,[(("src", "a"), Left (Q Qubit), ("X", "a"))
+           ,(("X", "b"), Left (Q Qubit), ("tgt", "b"))
+           ]
+          )
       c = [Cmd (Op "X" []) []]
   in processTest g (R [("in", Q Qubit)], R [("out", Q Qubit)]) c
 
+-- TODO: 
+testRx = testCase "Rx" $
+  let g = ([BratNode "id" (Prim "Rx")
+            [("th", SimpleTy FloatTy)]
+            [("kernel", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))]
+           ,BratNode "angle" (Prim "angle") [("th", SimpleTy FloatTy)] []
+           --,KernelNode "eval" (Eval "") testProcess
+           ,BratNode "main" ("src" :>>: "tgt") [] [("fun", K (R [("a", Q Qubit)]) (R [("b", Q Qubit)]))]
+           ,KernelNode "src" Source [] [("a", Q Qubit)]
+           ,KernelNode "tgt" Target [("b", Q Qubit)] []
+           ]
+          ,[]
+          )
+      c = [Cmd (Op "Rx" [{- angle?? -}]) []]
+  in processTest g (R [("in", Q Qubit)], R [("out", Q Qubit)]) c
 
-circuitTests = testGroup "Circuit" [testId]
+circuitTests = testGroup "Circuit" [testId, testId2, testX] -- not yet: , testRx]
