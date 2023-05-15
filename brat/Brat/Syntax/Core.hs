4a5b33f8d53f67e8e5519ec1ca3804bdee8d61d9
Craig Roy <craig.roy@cambridgequantum.com>
2023-05-15 10:08:52 +0100

    [cleanup] Remove unused abstractTm

--- brat/Brat/Syntax/Core.hs
+++ brat/Brat/Syntax/Core.hs
@@ -8,7 +8,6 @@ import Brat.Syntax.Common
 import Brat.Syntax.Simple
 import Brat.UserName
 
-import Data.Bifunctor (second)
 import Data.Kind (Type)
 
 type SType = SType' (Term Chk Noun)
@@ -96,43 +95,3 @@ instance Show (Term d k) where
 
   show (C f) = "{" ++ show f ++ "}"
   show (K (R ss) (R ts)) = showSig ss ++ " -o " ++ showSig ts
-
-abstractTm :: UserName -> Int -> Term d k -> Term d k
-abstractTm from to (Var x) | x == from = Inx to
-                           | otherwise = Var x
-abstractTm from to (a :|: b) = (abstractTm from to <$> a) :|: (abstractTm from to <$> b)
-abstractTm from to (Let abs x y)
-  | not (occursInAbstractor (show from) (unWC abs))
-  = Let abs (abstractTm from to <$> x) (abstractTm from to <$> y)
-abstractTm from to (Th tm) = Th $ abstractTm from to <$> tm
-abstractTm from to (Emb tm) = Emb $ abstractTm from to <$> tm
-abstractTm from to (Pull ps tm) = Pull ps (abstractTm from to <$> tm)
-abstractTm from to (f :$: a)
-  = (abstractTm from to <$> f) :$: (abstractTm from to <$> a)
-abstractTm from to (tm ::: ty) = (abstractTm from to <$> tm) ::: ty -- shold we do the type too?
-abstractTm from to (a :-: b)
-  = (abstractTm from to <$> a) :-: (abstractTm from to <$> b)
-abstractTm from to (abs :\: tm)
-  | not (occursInAbstractor (show from) (unWC abs))
-  = abs :\: (abstractTm from to <$> tm)
-abstractTm from to (Con c tm) = Con c (abstractTm from to <$> tm)
-{-
-abstractTm from to (C (WC fc (ss :-> ts)))
-  = let ss' = second (fmap (fmap (abstractTm from to))) <$> ss
-        ts' = second (fmap (fmap (abstractTm from to))) <$> ts
-    in C $ WC fc $ ss' :-> ts'
--}
-abstractTm from to (C (ss :-> ts))
-  = C $ (abstRow ss) :-> (abstRow ts)
- where
-  abstRow :: [(PortName, KindOr (Term Chk Noun))] -> [(PortName, KindOr (Term Chk Noun))]
-  abstRow = fmap (second (fmap (abstractTm from to)))
-
-abstractTm from to (K (R ss) (R ts))
-  = K (abstRow ss) (abstRow ts)
- where
-  abstRow xs = R $ second abstractSType <$> xs
-  abstractSType :: SType -> SType
-  abstractSType (Of ty n) = Of (abstractSType ty) (abstractTm from to n)
-  abstractSType sty = sty
-abstractTm _ _ tm = tm
