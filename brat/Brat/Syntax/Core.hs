f1f6e4c3ae460d98b30ce940d86b3a12e027f200
Craig Roy <croyzor@users.noreply.github.com>
2024-01-08 10:26:12 +0000

    [fix] Add `Show` for `Core.Arith` (#306)
    * [fix] Add `Show` for `Core.Arith`
    
    * [new] Add brackets to `Show` instance for `Term`
    
    * [fix] Bracket RHS of arithmetic operations
    
    * [new] Add brackets to `Show` for lambdas and compositions

--- brat/Brat/Syntax/Core.hs
+++ brat/Brat/Syntax/Core.hs
@@ -1,6 +1,13 @@
 {-# LANGUAGE FlexibleContexts #-}
 
-module Brat.Syntax.Core where
+module Brat.Syntax.Core (Term(..)
+                        ,SType
+                        ,Input
+                        ,Output
+                        ,InOut
+                        ,CType
+                        ,Decl
+                        ) where
 
 import Brat.FC
 import Brat.Naming
@@ -9,6 +16,7 @@ import Brat.Syntax.Simple
 import Brat.UserName
 
 import Data.Kind (Type)
+import Data.Maybe (fromJust)
 
 type SType = SType' (Term Chk Noun)
 
@@ -64,19 +72,37 @@ instance Show (Term d k) where
   show (VHole (MkName (name:_))) = '?' : show (MkName [name])
   show (VHole (MkName [])) = "?<root>"
   show Empty = "()"
-  show (a :|: b) = show a ++ ", " ++ show b
+  show (a :|: b) = bracket 2 a ++ ", " ++ bracket 2 b
   show (Th comp) = '{' : show comp ++ "}"
   show (TypedTh comp) = "{:" ++ show comp ++ ":}"
   show (Force th) = show th ++ "()"
   show (Forget kv) = "(Forget " ++ show kv ++ ")"
   show (Emb x) = '「' : show x ++ "」"
-  show (Pull [] x) = "[]:" ++ show x
-  show (Pull ps x) = concat ((++":") <$> ps) ++ show x
+  show tm@(Arith op lhs rhs) = let prec = fromJust (precedence tm) in
+                                 unwords [bracket prec lhs
+                                         ,showOp op
+                                         -- Arith ops are left nested, so call
+                                         -- the rhs with `prec + 1`
+                                         ,bracket (prec + 1) rhs
+                                         ]
+   where
+    showOp = \case
+      Add -> "+"
+      Mul -> "*"
+      Pow -> "^"
+      Sub -> "-"
+      Div -> "/"
+  show (Pull ps x) = showList ps ++ bracket 1 x
+   where
+    showList [] = "[]:"
+    showList ps = concatMap (++":") ps
+
   show (Var x) = show x
-  show (fun :$: arg) = show fun ++ ('(' : show arg ++ ")")
-  show (tm ::: ty) = show tm ++ " :: " ++ show ty
-  show (a :-: b) = show a ++ "; " ++ show b
-  show (xs :\: bod) = show xs ++ " => " ++ show bod
+  -- Nested applications should be bracketed too, hence 4 instead of 3
+  show (fun :$: arg) = bracket 4 fun ++ ('(' : show arg ++ ")")
+  show (tm ::: ty) = bracket 4 tm ++ " :: " ++ show ty
+  show (a :-: b) = bracket 2 a ++ "; " ++ bracket 3 b
+  show (xs :\: bod) = show xs ++ " => " ++ bracket 1 bod
   show p@(Con c arg) = case prettyPat p of
     Just ps -> show ps
     Nothing -> {-case unWC arg of
@@ -90,3 +116,26 @@ instance Show (Term d k) where
 
   show (C f) = "{" ++ show f ++ "}"
   show (K (R ss) (R ts)) = showSig ss ++ " -o " ++ showSig ts
+
+-- Wrap a term in brackets if its `precedence` is looser than `n`
+bracket :: Int -> WC (Term d k) -> String
+bracket n (WC _ tm) = case precedence tm of
+  Just m | m < n -> '(' : show tm ++ ")"
+  _ -> show tm
+
+-- Report tightness of binding, or `Nothing` if not a binary op
+precedence :: Term d k -> Maybe Int
+precedence (Let _ _ _) = Just 0
+precedence (_ :\: _) = Just 1
+precedence (_ :-: _) = Just 2
+precedence (Pull _ _) = Just 3
+precedence (_ :|: _) = Just 4
+precedence (_ :$: _) = Just 5
+precedence (_ ::: _) = Just 6
+precedence (Arith op _ _) = case op of
+  Add -> Just 7
+  Sub -> Just 7
+  Div -> Just 8
+  Mul -> Just 8
+  Pow -> Just 9
+precedence _ = Nothing
