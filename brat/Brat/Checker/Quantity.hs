290ea8319d417aa998493756b6024f96c7e6f775
Craig Roy <craig.roy@cambridgequantum.com>
2024-08-07 17:30:06 +0100

    Remove qpred

--- brat/Brat/Checker/Quantity.hs
+++ brat/Brat/Checker/Quantity.hs
@@ -1,8 +1,3 @@
 module Brat.Checker.Quantity where
 
-data Quantity = None | One | Tons deriving Show
-
-qpred :: Quantity -> Maybe Quantity
-qpred None = Nothing
-qpred One  = Just None
-qpred Tons = Just Tons
+data Quantity = None | One | Tons deriving (Enum, Show)
