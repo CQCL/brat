ccbeb1ea9044497674b23cdf6ff8c9e3c170ca0f
Alan Lawrence <alan.lawrence@cambridgequantum.com>
2024-10-25 12:59:48 +0100

    Move parseAndCheck from Test.Util into Test.Checking

--- brat/test/Test/Checking.hs
+++ brat/test/Test/Checking.hs
@@ -1,10 +1,14 @@
-module Test.Checking (getCheckingTests, expectedCheckingFails) where
+module Test.Checking (parseAndCheck, getCheckingTests, expectedCheckingFails) where
 
+import Brat.Load
+import Brat.Naming (root)
 import Test.Parsing (expectedParsingFails)
-import Test.Util (parseAndCheck, expectFailForPaths)
+import Test.Util (expectFailForPaths)
 
+import Control.Monad.Except
 import System.FilePath
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.Silver
 
 expectedCheckingFails = map ("examples" </>) ["nested-abstractors.brat"
@@ -17,3 +21,11 @@ parseAndCheckXF = expectFailForPaths (expectedParsingFails ++ expectedCheckingFa
 
 getCheckingTests :: IO TestTree
 getCheckingTests = testGroup "checking" . fmap parseAndCheckXF <$> findByExtension [".brat"] "examples"
+
+parseAndCheck :: [FilePath] -> FilePath -> TestTree
+parseAndCheck libDirs file = testCase (show file) $ do
+  env <- runExceptT $ loadFilename root libDirs file
+  case env of
+    Left err -> assertFailure (show err)
+    Right (venv, nouns, holes, _, _) ->
+      ((length venv) + (length nouns) + (length holes) > 0) @? "Should produce something"
--- brat/test/Test/Util.hs
+++ brat/test/Test/Util.hs
@@ -5,10 +5,8 @@ import Brat.Checker.Monad
 import Brat.Checker.Types (initStore, emptyEnv)
 import Brat.Error
 import Brat.FC
-import Brat.Load
 import Brat.Naming
 
-import Control.Monad.Except
 import Test.Tasty
 import Test.Tasty.HUnit
 import Test.Tasty.ExpectedFailure
@@ -19,14 +17,6 @@ assertChecking :: Checking a -> Assertion
 assertChecking m = case runEmpty $ localFC (FC (Pos 0 0) (Pos 0 0)) m of
   Right _ -> pure ()
   Left err -> assertFailure (showError err)
-
-parseAndCheck :: [FilePath] -> FilePath -> TestTree
-parseAndCheck libDirs file = testCase (show file) $ do
-  env <- runExceptT $ loadFilename root libDirs file
-  case env of
-    Left err -> assertFailure (show err)
-    Right (venv, nouns, holes, _, _) ->
-      ((length venv) + (length nouns) + (length holes) > 0) @? "Should produce something"
 
 expectFailForPaths :: [FilePath] -> (FilePath -> TestTree) -> FilePath -> TestTree
 expectFailForPaths xf f path = (if path `elem` xf then expectFail else id) $ f path
