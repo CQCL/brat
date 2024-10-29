cea653c26374e546e552e64c88bdbb3a240457e9
Alan Lawrence <alan.lawrence@quantinuum.com>
2024-10-29 09:45:15 +0000

    tests: improve expectFailForPaths (#46)
    * Move `expectFailForPaths` from Test/Parsing.hs to Test/Util.hs
    * Move `parseAndCheck` from Test/Util.hs to Test/Checking.hs
    * expectFailForPaths now checks that paths actually exist...
    * ...so remove a couple that didn't!

--- brat/test/Test/Checking.hs
+++ brat/test/Test/Checking.hs
@@ -1,10 +1,14 @@
-module Test.Checking (getCheckingTests, expectedCheckingFails) where
+module Test.Checking (parseAndCheck, getCheckingTests, expectedCheckingFails) where
 
-import Test.Parsing (expectedParsingFails, expectFailForPaths)
-import Test.Util (parseAndCheck)
+import Brat.Load
+import Brat.Naming (root)
+import Test.Parsing (expectedParsingFails)
+import Test.Util (expectFailForPaths)
 
+import Control.Monad.Except
 import System.FilePath
 import Test.Tasty
+import Test.Tasty.HUnit
 import Test.Tasty.Silver
 
 expectedCheckingFails = map ("examples" </>) ["nested-abstractors.brat"
@@ -12,8 +16,16 @@ expectedCheckingFails = map ("examples" </>) ["nested-abstractors.brat"
                                              ,"hea.brat"
                                              ]
 
-parseAndCheckXF :: FilePath -> TestTree
+parseAndCheckXF :: [FilePath] -> [TestTree]
 parseAndCheckXF = expectFailForPaths (expectedParsingFails ++ expectedCheckingFails) (parseAndCheck [])
 
 getCheckingTests :: IO TestTree
-getCheckingTests = testGroup "checking" . fmap parseAndCheckXF <$> findByExtension [".brat"] "examples"
+getCheckingTests = testGroup "checking" . parseAndCheckXF <$> findByExtension [".brat"] "examples"
+
+parseAndCheck :: [FilePath] -> FilePath -> TestTree
+parseAndCheck libDirs file = testCase (show file) $ do
+  env <- runExceptT $ loadFilename root libDirs file
+  case env of
+    Left err -> assertFailure (show err)
+    Right (venv, nouns, holes, _, _) ->
+      ((length venv) + (length nouns) + (length holes) > 0) @? "Should produce something"
