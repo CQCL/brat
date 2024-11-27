4e776cc8c9e48a50fb90e975756dd64e3f86bc49
Craig Roy <craig.roy@cambridgequantum.com>
2024-11-27 12:08:13 +0000

    XFAIL infer.brat because we don't compile `Pow`

--- brat/test/Test/Compile/Hugr.hs
+++ brat/test/Test/Compile/Hugr.hs
@@ -38,6 +38,7 @@ nonCompilingExamples = expectedCheckingFails ++ expectedParsingFails ++
   ,"fanout" -- Contains Selectors
   ,"vectorise" -- Generates MapFun nodes which aren't implemented yet
   ,"batcher-merge-sort" -- Generates MapFun nodes which aren't implemented yet
+  ,"infer" -- Generates `Pow` nodes which aren't implemented yet
   -- Victims of #13
   ,"arith"
   ,"cqcconf"
