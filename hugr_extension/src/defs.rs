15a6b4f9b1136ba476561e673903cf6980e14f40
Craig Roy <craig.roy@cambridgequantum.com>
2024-12-12 15:22:24 +0000

    Update extension & validator to hugr 0.8.0

--- hugr_extension/src/defs.rs
+++ hugr_extension/src/defs.rs
@@ -10,7 +10,7 @@ use hugr::{
     },
     ops::NamedOp,
     std_extensions::collections::list_type,
-    types::{type_param::TypeParam, FunctionType, PolyFuncType, Type, TypeArg, TypeBound},
+    types::{type_param::TypeParam, FunctionType, PolyFuncType, Type, TypeArg, TypeBound, TypeEnum},
 };
 
 use lazy_static::lazy_static;
@@ -111,8 +111,11 @@ impl SignatureFromArgs for HoleSigFun {
         // The Hole op expects a nat identifier and two type sequences specifiying
         // the signature of the hole
         match arg_values {
-            [TypeArg::BoundedNat { n: _ }, input, output] => {
-                Ok(FunctionType::new(row_from_arg(input)?, row_from_arg(output)?).into())
+            [TypeArg::BoundedNat { n: _ }, TypeArg::Type { ty: fun_ty }] => {
+                let TypeEnum::Function(sig) = fun_ty.as_type_enum().clone() else {
+                    return Err(SignatureError::InvalidTypeArgs);
+                };
+                Ok(PolyFuncType::new([], *sig))
             }
             _ => Err(SignatureError::InvalidTypeArgs),
         }
@@ -120,8 +123,8 @@ impl SignatureFromArgs for HoleSigFun {
 
     fn static_params(&self) -> &[TypeParam] {
         lazy_static! {
-            static ref PARAMS: [TypeParam; 3] =
-                [TypeParam::max_nat(), list_of_type(), list_of_type()];
+            static ref PARAMS: [TypeParam; 2] =
+                [TypeParam::max_nat(), TypeParam::Type { b: TypeBound::Any }];
         }
         PARAMS.as_slice()
     }
@@ -133,13 +136,15 @@ impl SignatureFromArgs for SubstituteSigFun {
     fn compute_signature(&self, arg_values: &[TypeArg]) -> Result<PolyFuncType, SignatureError> {
         // The Substitute op expects a function signature and a list of hole signatures
         match arg_values {
-            [fun_sig, TypeArg::Sequence { elems: hole_sigs }] => {
-                let fun_ty = Type::new_function(sig_from_arg(fun_sig)?);
-                let mut inputs = vec![fun_ty.clone()];
+            [TypeArg::Type { ty: outer_fun_ty }, TypeArg::Sequence { elems: hole_sigs }] => {
+                let mut inputs = vec![outer_fun_ty.clone()];
                 for sig in hole_sigs {
-                    inputs.push(Type::new_function(sig_from_arg(sig)?))
+                    let TypeArg::Type { ty: inner_fun_ty } = sig else {
+                        return Err(SignatureError::InvalidTypeArgs);
+                    };
+                    inputs.push(inner_fun_ty.clone())
                 }
-                Ok(FunctionType::new(inputs, vec![fun_ty]).into())
+                Ok(FunctionType::new(inputs, vec![outer_fun_ty.clone()]).into())
             }
             _ => Err(SignatureError::InvalidTypeArgs),
         }
@@ -148,9 +153,11 @@ impl SignatureFromArgs for SubstituteSigFun {
     fn static_params(&self) -> &[TypeParam] {
         lazy_static! {
             static ref PARAMS: [TypeParam; 2] = [
-                tuple_of_list_of_type(),
+                // The signature of outer functions
+                TypeParam::Type { b: TypeBound::Any },
+                // A list of signatures for the inner functions which fill in holes
                 TypeParam::List {
-                    param: Box::new(tuple_of_list_of_type())
+                    param: Box::new(TypeParam::Type { b: TypeBound::Any }),
                 },
             ];
         }
@@ -231,24 +238,8 @@ fn row_from_arg(arg: &TypeArg) -> Result<Vec<Type>, SignatureError> {
     }
 }
 
-fn sig_from_arg(arg: &TypeArg) -> Result<FunctionType, SignatureError> {
-    match arg {
-        TypeArg::Sequence { elems } if elems.len() == 2 => Ok(FunctionType::new(
-            row_from_arg(&elems[0])?,
-            row_from_arg(&elems[1])?,
-        )),
-        _ => Err(SignatureError::InvalidTypeArgs),
-    }
-}
-
 fn list_of_type() -> TypeParam {
     TypeParam::List {
         param: Box::new(TypeParam::Type { b: TypeBound::Any }),
-    }
-}
-
-fn tuple_of_list_of_type() -> TypeParam {
-    TypeParam::Tuple {
-        params: vec![list_of_type(), list_of_type()],
     }
 }
